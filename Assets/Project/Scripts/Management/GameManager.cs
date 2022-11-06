f1ebd029251c56c19eb0505e159748a7bb9350aa
adri2711 <adri271101@gmail.com>
2022-11-06 04:50:39 +0100

    base

---
+++ Assets/Project/Scripts/Management/GameManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState
+    {
+        MAINMENU,
+        SELECT,
+        PARSER
+    }
+
+    private GameState gameState = GameState.MAINMENU;
+    private int currLevel = 0;
+    [SerializeField] private Level[] levels;
+
+    public static GameManager instance { get; private set; }
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        switch (gameState)
+        {
+            case GameState.MAINMENU:
+                
+                break;
+            case GameState.SELECT:
+                
+                break;
+            case GameState.PARSER:
+                ParserUpdate();
+                break;
+        }
+    }
+
+    private void ParserStart()
+    {
+        if (currLevel < GetLevelCount())
+        {
+            levels[currLevel].LevelStart();
+        }
+    }
+    private void ParserUpdate()
+    {
+        if (currLevel < GetLevelCount())
+        {
+            levels[currLevel].LevelUpdate();
+        }
+    }
+
+    public int GetCurrentLevel()
+    {
+        return currLevel;
+    }
+    public int GetLevelCount()
+    {
+        return levels.Count();
+    }
+    public GameState GetGameState()
+    {
+        return gameState;
+    }
+}
---
+++ Assets/Project/Scripts/Management/Levels/Level1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1 : Level
+{
+    public override void LevelStart()
+    {
+        base.LevelStart();
+    }
+
+    public override void LevelUpdate()
+    {
+        base.LevelUpdate();
+    }
+}
