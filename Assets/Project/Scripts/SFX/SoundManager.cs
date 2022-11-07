986f204d92ce88910dc44cc88adb6b6c880c0772
adri2711 <adri271101@gmail.com>
2022-11-07 22:53:30 +0100

    scene & script

---
+++ Assets/Project/Scripts/SFX/SoundManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance { get; private set; }
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        GameEvents.instance.enterCorrectLetter += EnterCorrectLetterSound;
+        GameEvents.instance.enterWrongLetter += EnterWrongLetterSound;
+        GameEvents.instance.lose += LoseSound;
+        GameEvents.instance.finishLevel += FinishLevelSound;
+        GameEvents.instance.gainExtraTime += GainExtraTimeSound;
+        
+    }
+    
+    public void EnterCorrectLetterSound(int i)
+    {
+
+    }
+    public void EnterWrongLetterSound(int i)
+    {
+
+    }
+    public void LoseSound()
+    {
+
+    }
+    public void FinishLevelSound()
+    {
+
+    }
+    public void GainExtraTimeSound(float f)
+    {
+
+    }
+}
