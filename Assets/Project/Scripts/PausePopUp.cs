6ceb72d003527a51f0a4d3f4764e235984e7718e
Maxmonteyszaera <99646223+Maxmonteyszaera@users.noreply.github.com>
2022-11-10 22:53:30 +0100

    basic architecture
    :)

---
+++ Assets/Project/Scripts/PausePopUp.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausePopUp : MonoBehaviour
+{
+    public GameObject pauseScreeen;
+    public Animation animation;
+
+    public void Appear()
+    {
+        //animation.SetTrigger("appear");
+    }
+}
