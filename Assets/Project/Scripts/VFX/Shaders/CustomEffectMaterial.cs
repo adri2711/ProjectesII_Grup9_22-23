c41881e451c3cbcb05d3d0a73ff1b007e1171b15
adri2711 <adri271101@gmail.com>
2022-12-06 04:12:27 +0100

    cache property ids

--- Assets/Project/Scripts/VFX/Shaders/CustomEffectMaterial.cs
+++ Assets/Project/Scripts/VFX/Shaders/CustomEffectMaterial.cs
@@ -7,10 +7,19 @@ using UnityEngine;
 public class CustomEffectMaterial
 {
     public Material material;
-    //public int[] propertyIds;
+    public Dictionary<string, int> propertyIds = new Dictionary<string, int>();
+    private static HashSet<string> internalProperties = new HashSet<string>() {"_MainTex"};
 
     public CustomEffectMaterial(Material m)
     {
         material = m;
+        for (int i = 0; i < material.shader.GetPropertyCount(); i++)
+        {
+            string name = material.shader.GetPropertyName(i);
+            if (!internalProperties.Contains(name))
+            {
+                propertyIds[name] = Shader.PropertyToID(name);
+            }
+        }
     }
 }
