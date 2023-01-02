cf8e4b76e7b75e06810a52119386b6e91abdb37c
Iris LC <iris.lazaro@enti.cat>
2023-01-02 22:53:03 +0100

    Add bootscreens, sync them with audio

---
+++ Assets/Project/Scripts/MainMenu/Startup.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Startup : MonoBehaviour
+{
+    public AudioSource startupAudio;
+    public GameObject menu;
+    public Image bootScreen;
+
+    public Sprite bootScreenListing;
+    public Sprite bootScreenListingFinished;
+    public Sprite bootScreenFinished;
+
+    void Start()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    public IEnumerator StartGame()
+    {
+        Debug.Log("Coroutine start");
+        startupAudio.Play();
+
+        yield return new WaitForSeconds(0.5f);
+        bootScreen.sprite = bootScreenListing;
+
+        yield return new WaitForSeconds(2.5f);
+        bootScreen.sprite = bootScreenListingFinished;
+
+        yield return new WaitForSeconds(3.0f);
+        bootScreen.sprite = bootScreenFinished;
+
+        yield return new WaitForSeconds(2.0f);
+        Debug.Log("Show menu");
+        menu.SetActive(true);
+    }
+}
